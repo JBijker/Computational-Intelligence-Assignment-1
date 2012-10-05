0c0de180af09955946c649bc76339f627e423072
Jasper Henny <Jasper.Henny@gmail.com>
2012-10-05 14:43:34 +0200

    het is 15 man!

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int N = 5;
+		int N = 15;
 		
 		roadNetwork roads = new roadNetwork(N);
 		System.out.println("Aantal steden: " + roads.getN());
